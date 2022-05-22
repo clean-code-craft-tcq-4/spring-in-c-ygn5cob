89cbfc37249b51bf6533e3d875c59d8c5faa0fa1
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-05-22 21:10:32 +0530

    Create alerter.h

---
+++ alerter.h
@@ -0,0 +1,5 @@
+void emailAlerter(float maxThreshold, struct Stats computedStats);
+void ledAlerter(float maxThreshold, struct Stats computedStats);
+
+int emailAlertCallCount;
+int ledAlertCallCount;

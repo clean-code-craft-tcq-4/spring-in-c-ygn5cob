1b88cfecfee800c396f296e7284f9bfe8a710d55
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-05-22 20:54:36 +0530

    Update stats.c
    updated logic for min,max and average

--- stats.c
+++ stats.c
@@ -5,6 +5,34 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    int sum = 0;
+    
+    for(int i = 0; i< setlength; i++)
+    {
+       sum = sum + numberset[i];
+       if((s.max == 0) &&(s.min == 0))
+       {
+           s.max = numberset[i];
+           s.min = numberset[i];
+       }
+       else if(s.max < numberset[i])
+       {
+           s.max = numberset[i];
+       }
+       else if(s.min > numberset[i])
+       {
+           s.min = numberset[i]; 
+       }
+        else
+        {
+            //do nothing
+        }
+    }
+    s.average = sum/setlength;
+    
+    //maximum
+        
+
 }
 
 int emailAlertCallCount = 0;
